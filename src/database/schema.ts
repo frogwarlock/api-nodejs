1236f41cd4a275d621801b64eb0faa6fb41e3729
isabella.conceicao <isabella.conceicao@pucpr.br>
2025-12-05 18:44:18 -0300

    feat(database): add users schema and update dependencies
    - Added a new schema for users in the database using drizzle-orm.
    - Updated package.json to include drizzle-orm and pg as dependencies.
    - Added drizzle-kit as a dev dependency for database migrations.

---
+++ src/database/schema.ts
@@ -0,0 +1,9 @@
+import { text } from 'drizzle-orm/pg-core'
+import { pgTable, uuid } from 'drizzle-orm/pg-core'
+
+
+export const users = pgTable('users', {
+    id: uuid().primaryKey().defaultRandom(),
+    name: text().notNull(),
+    email: text().notNull().unique(),
+})
